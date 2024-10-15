9980a808d64b177b0e25c438610cdafc77cf4971
EV401SALVATION <donmharp@gmail.com>
2024-10-14 21:13:13 -0700

    10/14/24 Movement Finished
    Finished implementing basic top-down movement with zero gravity like physics.

--- PsycheIridiumBuildv0/Assets/Donovan Top Down/PlayerController.cs
+++ PsycheIridiumBuildv0/Assets/Donovan Top Down/PlayerController.cs
@@ -5,53 +5,97 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private InputActionAsset inputActions;
+    // Player Input
+    [SerializeField] private PlayerInput playerInput;
+
+    // Settings
+    public float moveSpeed;
+
+    // Sprites
+    [SerializeField] private Sprite upSprite;
+    [SerializeField] private Sprite downSprite;
+    [SerializeField] private Sprite leftSprite;
+    [SerializeField] private Sprite rightSprite;
+
+    // Other Player Components
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    // Input Actions
+    private InputActionAsset inputActions;
+    private InputAction upAction;
+    private InputAction downAction;
+    private InputAction leftAction;
+    private InputAction rightAction;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get input actions.
-        InputAction upAction = inputActions.FindActionMap("Top Down").FindAction("Up");
-        InputAction downAction = inputActions.FindActionMap("Top Down").FindAction("Down");
-        InputAction leftAction = inputActions.FindActionMap("Top Down").FindAction("Left");
-        InputAction rightAction = inputActions.FindActionMap("Top Down").FindAction("Right");
+        // Get the other needed components of the player.
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
-        // Assign action implementation to their actions.
-        upAction.performed += UpAction_performed;
-        downAction.performed += DownAction_performed;
-        leftAction.performed += LeftAction_performed;
-        rightAction.performed += RightAction_performed;
+        // Get the input actions asset.
+        inputActions = playerInput.actions;
 
-        // Enable input actions.
-        upAction.Enable();
-        downAction.Enable();
-        leftAction.Enable();
-        rightAction.Enable();
+        // Get the input actions.
+        upAction = inputActions.FindAction("Up");
+        downAction = inputActions.FindAction("Down");
+        leftAction = inputActions.FindAction("Left");
+        rightAction = inputActions.FindAction("Right");
+
+        // Set the initial sprite.
+        spriteRenderer.sprite = downSprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // X and Y direction variables.
+        int directionX;
+        int directionY;
+
+        // Determine the horizontal direction of velocity.
+        if (leftAction.inProgress && !rightAction.inProgress) directionX = -1;
+        else if (!leftAction.inProgress && rightAction.inProgress) directionX = 1;
+        else directionX = 0;
+
+        // Determine the vertical direction of velocity.
+        if (upAction.inProgress && !downAction.inProgress) directionY = 1;
+        else if (!upAction.inProgress && downAction.inProgress) directionY = -1;
+        else directionY = 0;
+
+        // Set the correct sprite (vertical priority).
+        if (directionX < 0) spriteRenderer.sprite = rightSprite;
+        else if (directionX > 0) spriteRenderer.sprite = leftSprite;
+        if (directionY < 0) spriteRenderer.sprite = downSprite;
+        else if (directionY > 0) spriteRenderer.sprite = upSprite;
+
+        // Direction of Velocity
+        Vector3 direction = new Vector3(directionX, directionY, 0);
+
+        // Add force to the player.
+        rb.AddForce(direction * moveSpeed);
     }
 
-    private void UpAction_performed(InputAction.CallbackContext obj)
+    // Unused event callbacks.
+    public void UpAction_performed(InputAction.CallbackContext context)
     {
-        Debug.Log("Up");
+
     }
 
-    private void DownAction_performed(InputAction.CallbackContext obj)
+    public void DownAction_performed(InputAction.CallbackContext context)
     {
-        Debug.Log("Down");
+
     }
 
-    private void LeftAction_performed(InputAction.CallbackContext obj)
+    public void LeftAction_performed(InputAction.CallbackContext context)
     {
-        Debug.Log("Left");
+
     }
 
-    private void RightAction_performed(InputAction.CallbackContext obj)
+    public void RightAction_performed(InputAction.CallbackContext context)
     {
-        Debug.Log("Right");
+
     }
 }
